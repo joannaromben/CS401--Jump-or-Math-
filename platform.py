8a15e6adab7b89a874bf62891591d1d3c6fd69c4
joannaromben <jromero_1291@email.ric.edu>
2024-10-26 17:32:46 -0400

    Update platform.py

--- platform.py
+++ platform.py
@@ -1,9 +1,1 @@
-class Platform(pygame.sprite.Sprite):
-    def __init__(self, xloc, yloc, imgw, imgh, img):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(os.path.join('images', img)).convert()
-        self.image.convert_alpha()
-        self.image.set_colorkey(ALPHA)
-        self.rect = self.image.get_rect()
-        self.rect.y = yloc
-        self.rect.x = xloc
+
